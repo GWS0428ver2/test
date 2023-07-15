f31b8ec4c8c3db87d8dbe4a53792b46c37d96171
Wooseok Gwak <wooseok428@gmail.com>
2023-07-15 19:47:06 -0400

    Update main.py by adding print_user1 function

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def print_hi(name):
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def print_user1():
+    print('user1')
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_user1()
 
