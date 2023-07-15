870b4aded8324de0ff00aec9c7dabef30b18d887
user2 <wooseok428ver2@gmail.com>
2023-07-15 19:44:14 -0400

    Update main.py by adding print_user2 function

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+def print_user2():
+    print('User2')
+
 
 if __name__ == '__main__':
     print_hi('PyCharm')
-
+    print_user2()
