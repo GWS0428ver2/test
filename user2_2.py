878a9d45c72ccfc2579c28277e07fa6721c49774
user2 <wooseok428ver2@gmail.com>
2023-07-15 23:18:21 -0400

    Update user2_2.py by adding function to generate random date

--- user2_2.py
+++ user2_2.py
@@ -6,3 +6,9 @@ def print_randint():
 def print_randn():
     print(random.random())
 
+def print_random_date():
+    print(f"{random.randint(1, 2023):04d}-{random.randint(1, 12):02d}-{random.randint(1, 31):02d}")
+
+
+if __name__ == '__main__':
+    print_random_date()
